da90e5fbf039dbe7cb1d435e9c6bfbb39c9546b9
Julien Rouhaud <julien.rouhaud@free.fr>
2022-08-06 19:42:19 +0800

    Add regression test for utility statements.

--- test/sql/pg_show_rewritten_query.sql
+++ test/sql/pg_show_rewritten_query.sql
@@ -4,3 +4,4 @@ CREATE VIEW v_query_def AS SELECT dpt, sum(nb) AS sum_nb
 FROM t_query_def WHERE dpt IS NOT NULL GROUP BY dpt;
 
 SELECT pg_show_rewritten_query('SELECT * FROM v_query_def');
+SELECT pg_show_rewritten_query('CREATE TABLE t(id integer)');
